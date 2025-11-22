c67d8848c2be2fcf4cb7eaee39479fb78edacd84
ishakdoganay <zekishakdoganay@gmail.com>
2025-11-22 20:09:05 +0300

    fix error

--- Nexora.Data.Domain/EntityBuilders/ConsumerBuilder.cs
+++ Nexora.Data.Domain/EntityBuilders/ConsumerBuilder.cs
@@ -19,7 +19,13 @@ namespace Nexora.Data.Domain.EntityBuilders
             builder.Property(e => e.AccessToken).HasMaxLength(2000);
             builder.Property(e => e.RefreshToken).HasMaxLength(2000);
             builder.Property(c => c.Email).HasMaxLength(255).IsRequired();
-
+            builder.Property(c => c.Status)
+              .HasConversion<byte>()
+              .HasColumnType("tinyint");
+            // Also check Gender similarly if needed:
+            builder.Property(c => c.Gender)
+                   .HasConversion<byte?>()
+                   .HasColumnType("tinyint");
             builder
                 .HasIndex(c => new { c.Email, c.DeletedDate })
                 .HasDatabaseName("IX_Consumer_Email_DeletedDate");
